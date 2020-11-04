61fa3cc127115d92b14eab48438980ab05de9bfe
Ashur Cabrera <ashur@panic.com>
2020-11-04 11:02:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# nova-sketch
+
+[![](https://nova.app/badges/clone.svg)][clone]
+
+Run [Sketch plugin][plugins] commands from [Nova].
+
+[clone]: nova://clone?url=https://gitlab.com/ashur/nova-sketch.git
+[plugins]: https://developer.sketch.com/plugins
+[Nova]: https://nova.app
+
+## Usage
+
+Let's say we're using Nova to develop a Sketch plugin for importing and exporting palettes.
+
+Our plugin has a couple of commands, **Import Palette...** and **Export Palette...**
+
+Testing changes to either command's handler might look like this:
+
+1. Make changes in Nova
+1. Switch back to Sketch
+1. Open **Plugins → Palettes**
+1. Select the command to run it
+
+Not bad, but we can still remove a _little_ bit of friction in our development workflow.
+
+Using the [dynamic task assistant] feature added in Nova 2, this extension detects Sketch plugin commands defined in `Contents/Sketch/manifest.json`:
+
+```json
+"commands": [
+    {
+        "name": "Import Palette...",
+        "script": "script.js",
+        "handlers": {
+            "run": "importPalette"
+        },
+        "identifier": "cab.ashur.Sketch.importPalette"
+    },
+    {
+        "name": "Export Palette...",
+        "script": "script.js",
+        "handlers": {
+            "run": "exportPalette"
+        },
+        "identifier": "cab.ashur.Sketch.exportPalette"
+    }
+],
+```
+
+and automatically turns them into Tasks:
+
+![](/assets/tasks.png)
+
+Now we can run our Sketch plugin commands with a single click, keyboard shortcut, or command palette invocation ✨💎
+
+[dynamic task assistant]: https://docs.nova.app/api-reference/assistants-registry/#registertaskassistant-object-options
+
+## Setup
+
+1. [Download][download] and unzip this extension
+1. Double-click `Run in Sketch.novaextension`
+1. Click **Install**
+
+[download]: https://gitlab.com/ashur/nova-sketch/-/archive/main/nova-sketch-main.zip
+
+### Configuration
+
+By default, _Run in Sketch_ looks for Sketch in the `/Applications` folder. If you have it installed in a different location, you can specify the path in the extension preferences by opening **Extensions → Extension Library... → Run in Sketch → Preferences**:
+
+![](/assets/preferences.png)
---
+++ Run in Sketch.novaextension/scripts/main.js
@@ -0,0 +1,107 @@
+let didNotify = false;
+
+class TaskProvider
+{
+	constructor()
+	{
+		this.manifestPath = nova.path.join( nova.workspace.path, "Contents/Sketch/manifest.json" );
+		this.manifest = {};
+
+		nova.fs.watch( "Contents/Sketch/manifest.json", () =>
+		{
+			if( nova.fs.stat( this.manifestPath ) )
+			{
+				this.readManifest();
+				nova.workspace.reloadTasks( "cab.ashur.RunInSketch" );
+			}
+		});
+
+		this.bundlePath = nova.config.get( "cab.ashur.RunInSketch.bundlePath" );
+		nova.config.observe( "cab.ashur.RunInSketch.bundlePath", () =>
+		{
+			didNotify = false;
+			this.bundlePath = nova.config.get( "cab.ashur.RunInSketch.bundlePath" );
+			nova.workspace.reloadTasks( "cab.ashur.RunInSketch" );
+		});
+
+		this.readManifest();
+	}
+
+	readManifest()
+	{
+		if( nova.fs.stat( this.manifestPath ) )
+		{
+			let manifestFile = nova.fs.open( this.manifestPath, "r" );
+			let manifestJSON = manifestFile.read();
+			manifestFile.close();
+
+			this.manifest = JSON.parse( manifestJSON );
+		}
+	}
+
+	/**
+	 * @returns {Promise<Task[]>}
+	 */
+	async provideTasks()
+	{
+		let processCommand = `${this.bundlePath}/Contents/Resources/sketchtool/bin/sketchtool`;
+		if( !nova.fs.stat( processCommand ) )
+		{
+			if( !didNotify )
+			{				
+				let request = new NotificationRequest( "cab.ashur.RunInSketch-bundleNotFound" );
+				request.title = "Sketch Not Found";
+				request.body = `The Sketch application bundle could not be found at ‘${this.bundlePath}’.\n\nSpecify a new path in Preferences.`;
+				request.actions = ["OK", "Extension Preferences..."];
+				nova.notifications.add( request )
+					.then( response =>
+					{
+						if( response.actionIdx === 1 )
+						{
+							nova.openConfig( "cab.ashur.RunInSketch" );
+						}
+					})
+					.catch( error =>
+					{
+						console.error( error );
+						console.error( error.stack );
+					});
+	
+				didNotify = true;
+			}
+
+			return [];
+		}
+
+		try
+		{
+			let tasks = [];
+			if( this.manifest.commands && Array.isArray( this.manifest.commands ) )
+			{
+				tasks = this.manifest.commands.map( sketchCommand =>
+				{
+					if( sketchCommand.name )
+					{
+						let task = new Task( sketchCommand.name );
+						task.setAction( Task.Run, new TaskProcessAction( processCommand, {
+							args: ["run", nova.workspace.path, sketchCommand.identifier],
+							env: {},
+						} ) );
+
+						return task;						
+					}
+				});
+			}
+
+			return tasks.filter( task => task );
+		}
+		catch( error )
+		{
+			console.error( error );
+			console.error( error.stack );
+		}
+	}
+}
+
+let taskProvider = new TaskProvider();
+nova.assistants.registerTaskAssistant( taskProvider, { identifier: "cab.ashur.RunInSketch" } );
